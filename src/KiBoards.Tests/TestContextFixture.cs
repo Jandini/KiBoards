674afe80139e2ebce213b406187ab9e29983a4b0
Matt <matt.janda.kingston@gmail.com>
2023-10-14 11:26:01 +0100

    Code cleanup

--- src/KiBoards.Tests/TestContextFixture.cs
+++ src/KiBoards.Tests/TestContextFixture.cs
@@ -7,7 +7,7 @@
             SetContext(new { 
                 Version = "9.0.0-alpha.123",
                 Computer = Environment.MachineName,
-                BranchName = Environment.GetEnvironmentVariable("BRANCH_NAME")                
+                BranchName = Environment.GetEnvironmentVariable("BRANCH_NAME")
             });
         }
     }
--- src/KiBoards/Services/IKiBoardsElasticService.cs
+++ src/KiBoards/Services/IKiBoardsElasticService.cs
@@ -3,6 +3,5 @@
     internal interface IKiBoardsElasticService
     {
         Task IndexDocumentAsync<T>(T document) where T : class;
-
     }
 }
