f96b650cac3ececd06edd03b086bae9ddb439677
Matt <matt.janda.kingston@gmail.com>
2023-10-15 22:38:32 +0100

    Add message sink extensions to log string and exceptions

--- src/KiBoards.Xunit/TestExtensions.cs
+++ src/KiBoards.Xunit/TestExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace KiBoards
 {
     internal static class TestExtensions
     {     
         public static string ComputeMD5(this string value) => BitConverter.ToString(MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(value))).Replace("-", "").ToLower();
+        public static void WriteMessage(this IMessageSink messageSink, string message) => messageSink.OnMessage(new DiagnosticMessage(message));
+        public static void WriteException(this IMessageSink messageSink, Exception exception) => messageSink.OnMessage(new DiagnosticMessage("{0}\n{1}", exception.Message, exception.StackTrace));
     }
 }
