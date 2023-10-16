0fc1c015a480493d0dd63adb5589ba6452ca0d08
Matt Janda <matt.janda.kingston@gmail.com>
2023-10-16 23:19:45 +0100

    Add time to test case run (#20)
    * Add started and finshed at fields to test case run
    
    * Update kiboards project file
    
    * Update readme file

--- src/KiBoards.Xunit/Models/KiBoardsTestCaseRun.cs
+++ src/KiBoards.Xunit/Models/KiBoardsTestCaseRun.cs
@@ -2,13 +2,16 @@
 {
     class KiBoardsTestCaseRun
     {
-        public string Id { get; internal set; }
+        public string Id { get; set; }
+        public DateTime StartedAt { get; set; }
+        public DateTime FinishedAt { get; set; }
         public KiBoardsTestRun TestRun { get; set; }
         public KiBoardsTestCase TestCase { get; set; }
         public KiBoardsTestCaseRunSkipped Skipped { get; set; }
         public KiBoardsTestCaseRunFailed Failed { get; set; }
         public decimal ExecutionTime { get; set; }
         public string Status { get; set; }
-        public string Output { get; set; }        
+        public string Output { get; set; }
+        
     }
 }
