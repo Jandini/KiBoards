524795b402fce89019c45442376d707d1f7098b1
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-04 11:33:55 +0530

    Add logger to test bat

--- test.bat
+++ test.bat
@@ -4,6 +4,8 @@ set KIB_KIBANA_HOST=http://localhost:5601
 set KIB_DARK_MODE=1
 set KIB_B64_HELLO_WORLD=SGVsbG8KV29ybGQ=
 dotnet build -c:Release src\KiBoards.sln
-dotnet test -c:Release --no-build src\TestFramework
-dotnet test -c:Release --no-build src\TestObject
-dotnet test -c:Release --no-build src\TestStartup
+dotnet test --logger "console;verbosity=detailed" -c:Release --no-build src\TestFramework
+dotnet test --logger "console;verbosity=detailed" -c:Release --no-build src\TestObject
+dotnet test --logger "console;verbosity=detailed" -c:Release --no-build src\TestStartup
+
+
