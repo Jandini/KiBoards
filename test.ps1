101350f2bdd7a70713b574aa1584d9c3434c6152
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-04 09:59:37 +0530

    Add multi line hello world test variable

--- test.ps1
+++ test.ps1
@@ -1,6 +1,7 @@
 $env:KIB_ELASTICSEARCH_HOST='http://localhost:9200'
 $env:KIB_KIBANA_HOST='http://localhost:5601'
 $env:KIB_DARK_MODE=1
+$env:KIB_B64_HELLO_WORLD='SGVsbG8KV29ybGQ='
 dotnet build -c:Release src\KiBoards.sln
 dotnet test -c:Release --no-build src\TestFramework --logger="console"
 dotnet test -c:Release --no-build src\TestObject
