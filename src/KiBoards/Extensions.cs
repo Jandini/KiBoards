9c91a7a058920e72f7054d27ac716f6754f199b9
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-16 19:28:56 +0530

    Add KIB_KIBANA_ACCEPT_ANY_CERT environment and create prerelease nugget

--- src/KiBoards/Extensions.cs
+++ src/KiBoards/Extensions.cs
@@ -7,6 +7,6 @@ namespace KiBoards
     internal static class Extensions
     {     
         public static void WriteMessage(this IMessageSink messageSink, string message, [CallerMemberName] string method = "") => messageSink.OnMessage(new DiagnosticMessage($"{method}: {message}"));
-        public static void WriteException(this IMessageSink messageSink, string message, Exception exception, [CallerMemberName] string method = "") => messageSink.OnMessage(new DiagnosticMessage($"{message}\n{exception.Message}{(exception.InnerException != null ? "\n" + exception.InnerException.Message : "")}"));
+        public static void WriteException(this IMessageSink messageSink, string message, Exception exception, [CallerMemberName] string method = "") => messageSink.OnMessage(new DiagnosticMessage($"{message} {exception.Message}{(exception.InnerException != null ? "\n" + exception.InnerException.Message : "")}"));
     }
 }
--- src/KiBoards/Startup.cs
+++ src/KiBoards/Startup.cs
@@ -22,96 +22,108 @@ namespace KiBoards
 
                 var task = Task.Run(async () =>
                 {
-                    var kibanaUri = new Uri(Environment.GetEnvironmentVariable("KIB_KIBANA_HOST") ?? "http://localhost:5601");
-
-                    var httpClient = new HttpClient()
+                    try
                     {
-                        BaseAddress = kibanaUri,
-                    };
-
-                    var kibanaClient = new KibanaHttpClient(httpClient);
-
-                    messageSink.WriteMessage($"Waiting for Kibana {kibanaUri}");
-
-                    while (true)
-                    {
-                        try
+                        var kibanaUri = new Uri(Environment.GetEnvironmentVariable("KIB_KIBANA_HOST") ?? "http://localhost:5601");
+                        var kibanaAcceptAnyCert = Environment.GetEnvironmentVariable("KIB_KIBANA_ACCEPT_ANY_CERT")?.Trim() switch
                         {
-                            messageSink.WriteMessage("Checking Kibana status");
-                            var response = await kibanaClient.GetStatus(CancellationToken.None);
+                            "1" or "true" => true,
+                            "0" or "false" => false,
+                            _ => false
+                        };
 
-                            string level = response?.Status?.Overall?.Level ?? throw new Exception("Kibana status is not available.");
+                        var httpClient = kibanaAcceptAnyCert 
+                            ? new HttpClient(new HttpClientHandler() { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator }) { BaseAddress = kibanaUri } 
+                            : new HttpClient() { BaseAddress = kibanaUri };
 
-                            if (level != "available")
-                                throw new Exception("Kibana not available.");
+                        var kibanaClient = new KibanaHttpClient(httpClient);
 
-                            messageSink.WriteMessage($"Kibana status: {level}");
-                            break;
+                        messageSink.WriteMessage($"Waiting for Kibana {kibanaUri}");
+
+                        while (true)
+                        {
+                            try
+                            {
+                                messageSink.WriteMessage("Checking Kibana status");
+                                var response = await kibanaClient.GetStatus(CancellationToken.None);
+
+                                string level = response?.Status?.Overall?.Level ?? throw new Exception("Kibana status is not available.");
+
+                                if (level != "available")
+                                    throw new Exception("Kibana not available.");
+
+                                messageSink.WriteMessage($"Kibana status: {level}");
+                                break;
+                            }
+                            catch (Exception ex)
+                            {
+                                messageSink.WriteException("Checking Kibana status failed.", ex);
+                                await Task.Delay(5000);
+                            }
                         }
-                        catch (Exception ex)
+                        messageSink.WriteMessage($"Trying to create Kibana space for KiBoards...");
+
+                        var kiboards = KibanaSpace.Create(
+                            id: "kiboards",
+                            name: GetEnvironmentVariable("KIB_SPACE_NAME", "KiBoards"),
+                            initials: GetEnvironmentVariable("KIB_SPACE_INITIALS", "Ki"),
+                            color: GetEnvironmentVariable("KIB_SPACE_COLOR", "#000000"),
+                            // enterpriseSearch, observabilityCases, discover,,logs,infrastructure,apm,uptime,slo,siem,securitySolutionCases,canvas,maps,ml,visualize,dev_tools,advancedSettings,indexPatterns,filesManagement,filesSharedImage,savedObjectsManagement,savedObjectsTagging,osquery,actions,generalCases,guidedOnboardingFeature,rulesSettings,maintenanceWindow,stackAlerts,fleetv2,fleet,monitoring
+                            disabledFeatures: Environment.GetEnvironmentVariable("KIB_DISABLE_FEATURES"),
+                            imageUrl: "",
+                            description: GetEnvironmentVariable("KIB_SPACE_DESCRIPTION", "KiBoards dashboards")
+                        );
+
+                        var result = await kibanaClient.CreateSpaceAsync(kiboards);
+
+                        if (result.StatusCode == HttpStatusCode.Conflict)
                         {
-                            messageSink.WriteException("Checking Kibana status failed.", ex);
-                            await Task.Delay(5000);
+                            messageSink.WriteMessage($"KiBoards space already exist. Updating the space...");
+                            result = await kibanaClient.UpdateSpaceAsync(kiboards);
+                        }
+
+                        if (result.IsSuccessStatusCode)
+                            messageSink.WriteMessage($"KiBoards space configured successfully.");
+                        else
+                            messageSink.WriteMessage($"KiBoards space failed to configure due to {result.ReasonPhrase}");
+
+                        var defaultRoute = GetEnvironmentVariable("KIB_DEFAULT_ROUTE", "/app/dashboards");
+                        messageSink.WriteMessage($"Configuring default route to {defaultRoute}");
+                        result = await kibanaClient.SetDefaultRoute(defaultRoute, kiboards.Id, CancellationToken.None);
+
+                        if (result.IsSuccessStatusCode)
+                            messageSink.WriteMessage($"KiBoards default route configured successfully.");
+                        else
+                            messageSink.WriteMessage($"KiBoards default route configuration failed due to {result.ReasonPhrase}.");
+
+
+                        var darkModeVariable = GetEnvironmentVariable("KIB_DARK_MODE", "0");
+                        var darkMode = darkModeVariable == "1" || darkModeVariable.ToLower() == "true";
+                        messageSink.WriteMessage($"{(darkMode ? "Enabling" : "Disabling")} Kibana dark mode.");
+                        await kibanaClient.SetDarkModeAsync(darkMode, KibanaSpace.KiBoards.Id, CancellationToken.None);
+
+                        var ndjsonFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), attribute.SearchPattern);
+
+                        messageSink.WriteMessage($"Found {ndjsonFiles.Length} ndjson file(s)");
+
+                        foreach (var ndjsonFile in ndjsonFiles.OrderBy(a => a))
+                        {
+                            string fileName = Path.GetFileName(ndjsonFile);
+
+                            messageSink.WriteMessage($"Importing {fileName}");
+
+                            var spaceId = fileName == "KiBoards.ndjson" ? "kiboards" : null;
+
+                            var results = await kibanaClient.ImportSavedObjectsAsync(ndjsonFile, attribute.Overwrite, spaceId);
+                            messageSink.WriteMessage($"Imported {results.SuccessCount} object(s)");
+
+                            if (!results.Success && results.SuccessCount > 0)
+                                messageSink.WriteMessage("Warning: Some objects were not imported. Please ensure proper import order based on their dependencies.");
                         }
                     }
-                    messageSink.WriteMessage($"Trying to create Kibana space for KiBoards...");
-
-                    var kiboards = KibanaSpace.Create(
-                        id: "kiboards",
-                        name: GetEnvironmentVariable("KIB_SPACE_NAME", "KiBoards"),
-                        initials: GetEnvironmentVariable("KIB_SPACE_INITIALS", "Ki"),
-                        color: GetEnvironmentVariable("KIB_SPACE_COLOR", "#000000"),
-                        // enterpriseSearch, observabilityCases, discover,,logs,infrastructure,apm,uptime,slo,siem,securitySolutionCases,canvas,maps,ml,visualize,dev_tools,advancedSettings,indexPatterns,filesManagement,filesSharedImage,savedObjectsManagement,savedObjectsTagging,osquery,actions,generalCases,guidedOnboardingFeature,rulesSettings,maintenanceWindow,stackAlerts,fleetv2,fleet,monitoring
-                        disabledFeatures: Environment.GetEnvironmentVariable("KIB_DISABLE_FEATURES"),
-                        imageUrl: "",
-                        description: GetEnvironmentVariable("KIB_SPACE_DESCRIPTION", "KiBoards dashboards")
-                    );
-
-                    var result = await kibanaClient.CreateSpaceAsync(kiboards);
-
-                    if (result.StatusCode == HttpStatusCode.Conflict)
+                    catch (Exception ex)
                     {
-                        messageSink.WriteMessage($"KiBoards space already exist. Updating the space...");
-                        result = await kibanaClient.UpdateSpaceAsync(kiboards);
-                    }
-
-                    if (result.IsSuccessStatusCode)
-                        messageSink.WriteMessage($"KiBoards space configured successfully.");
-                    else
-                        messageSink.WriteMessage($"KiBoards space failed to configure due to {result.ReasonPhrase}");
-
-                    var defaultRoute = GetEnvironmentVariable("KIB_DEFAULT_ROUTE", "/app/dashboards");
-                    messageSink.WriteMessage($"Configuring default route to {defaultRoute}");
-                    result = await kibanaClient.SetDefaultRoute(defaultRoute, kiboards.Id, CancellationToken.None);
-
-                    if (result.IsSuccessStatusCode)
-                        messageSink.WriteMessage($"KiBoards default route configured successfully.");
-                    else
-                        messageSink.WriteMessage($"KiBoards default route configuration failed due to {result.ReasonPhrase}.");
-
-
-                    var darkModeVariable = GetEnvironmentVariable("KIB_DARK_MODE", "0");
-                    var darkMode = darkModeVariable == "1" || darkModeVariable.ToLower() == "true";
-                    messageSink.WriteMessage($"{(darkMode ? "Enabling" : "Disabling")} Kibana dark mode.");
-                    await kibanaClient.SetDarkModeAsync(darkMode, KibanaSpace.KiBoards.Id, CancellationToken.None);
-
-                    var ndjsonFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), attribute.SearchPattern);
-
-                    messageSink.WriteMessage($"Found {ndjsonFiles.Length} ndjson file(s)");
-
-                    foreach (var ndjsonFile in ndjsonFiles.OrderBy(a => a))
-                    {
-                        string fileName = Path.GetFileName(ndjsonFile);
-
-                        messageSink.WriteMessage($"Importing {fileName}");
-
-                        var spaceId = fileName == "KiBoards.ndjson" ? "kiboards" : null;
-
-                        var results = await kibanaClient.ImportSavedObjectsAsync(ndjsonFile, attribute.Overwrite, spaceId);
-                        messageSink.WriteMessage($"Imported {results.SuccessCount} object(s)");
-
-                        if (!results.Success && results.SuccessCount > 0)
-                            messageSink.WriteMessage("Warning: Some objects were not imported. Please ensure proper import order based on their dependencies.");
+                        messageSink.WriteException("KiBoards failed.", ex);
                     }
                 });
             }
