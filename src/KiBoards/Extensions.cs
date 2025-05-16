b0064bf4481ddf8ddf6ba23c5a45876d34421f5a
Matt Janda <matt.janda.kingston@gmail.com>
2025-05-16 20:00:52 +0530

    Changed caller name to Startup and create prerelease nugget

--- src/KiBoards/Extensions.cs
+++ src/KiBoards/Extensions.cs
@@ -1,12 +1,11 @@
-﻿using System.Runtime.CompilerServices;
-using Xunit.Abstractions;
+﻿using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace KiBoards
 {
     internal static class Extensions
     {     
-        public static void WriteMessage(this IMessageSink messageSink, string message, [CallerMemberName] string method = "") => messageSink.OnMessage(new DiagnosticMessage($"{method}: {message}"));
-        public static void WriteException(this IMessageSink messageSink, string message, Exception exception, [CallerMemberName] string method = "") => messageSink.OnMessage(new DiagnosticMessage($"{message} {exception.Message}{(exception.InnerException != null ? "\n" + exception.InnerException.Message : "")}"));
+        public static void WriteMessage(this IMessageSink messageSink, string message, string method = "Startup") => messageSink.OnMessage(new DiagnosticMessage($"{method}: {message}"));
+        public static void WriteException(this IMessageSink messageSink, string message, Exception exception, string method = "Startup") => messageSink.OnMessage(new DiagnosticMessage($"{message} {exception.Message}{(exception.InnerException != null ? "\n" + exception.InnerException.Message : "")}"));
     }
 }
